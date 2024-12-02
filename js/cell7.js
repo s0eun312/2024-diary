f424a0d166c373cf55344a8a7267f74b8728ff5e
Soeun Ryu <soeunl131@gmail.com>
2024-12-02 23:30:36 +0900

    cell7 edit

--- js/cell7.js
+++ js/cell7.js
@@ -10,21 +10,16 @@ toppings.forEach(topping => {
 });
 
 btn.addEventListener('click', () => {
-    // 오답 처리
-    let isValid = true;
+    const selectedToppings = document.querySelectorAll(".topping.select");
+    const correctToppings = document.querySelectorAll(".topping.correct.select");
 
-    // 정답 처리
-    toppings.forEach(topping => {
-        // 선택된 토핑들 중 correct 클래스를 가진 div
-        if (topping.classList.contains('select') && !topping.classList.contains('correct')) {
-            isValid = false;
-        }
-    });
-
-    if (!isValid) {
+    if (selectedToppings.length !== correctToppings.length || correctToppings.length !== 3) {
+        // 1. 선택 토핑수와 정답 토핑 수가 다를 경우
+        // 2. 정답 토핑 수가 3개가 아닐 경우
+        // 오답 처리: 경고창 띄우기
         alert("이 조합은 내 취향이 아닌 것 같다...ㅜㅡㅜ");
     } else {
-        // 정답 후 이미지 전환 효과
+        // 정답 처리: 이미지 전환
         img.style.transition = "opacity 0.5s ease";
         img.style.opacity = 0;
 
